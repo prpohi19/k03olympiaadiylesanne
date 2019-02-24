2a6a55ca8fc10af201579c275e21d491611b3394
raimop <43129557+raimop@users.noreply.github.com>
2019-02-24 20:32:53 +0200

    Create Olympiad.java

---
+++ Olympiad.java
@@ -0,0 +1,41 @@
+package tund3;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Olympiad {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new FileReader("input0.txt"));
+        List<String> list = new ArrayList<>();
+
+        String row = reader.readLine();
+
+        while (row != null){
+            list.add(row);
+            row = reader.readLine();
+        }
+        
+        PrintWriter writer = new PrintWriter(new FileWriter("output0.txt"));
+
+        String lastValue = list.get(list.size()-1).replaceAll("\\*", ".");
+        list.remove(list.size()-1);
+
+        int counter = 0;
+
+        for (String letter: list){
+            if (letter.matches(lastValue)){ counter += 1; }
+        }
+
+        writer.println(counter);
+
+        for (String letter: list){
+            if (letter.matches(lastValue)){ writer.println(letter); }
+        }
+
+        writer.close();
+    }
+}
